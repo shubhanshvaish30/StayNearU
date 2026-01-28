f2b8894bfcd6a390490aad62d594440b8973feb5
Shubhansh Vaish <122173772+shubhanshvaish30@users.noreply.github.com>
2026-01-28 20:23:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app=express();
 
+
 const corsOptions={
     // origin:'https://stay-near-u-qffm.vercel.app',
     // origin: 'https://stay-near-u-8ryh-e86fyc8gf-shatakshi-sharmas-projects.vercel.app/',
